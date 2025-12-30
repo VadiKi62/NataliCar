a80699859401defac36796c8fd3f0a75de2086cd
Natalia Kirieieva <nataliakireewa@gmail.com>
2025-12-30 00:46:29 +0000

    chore: удалены неиспользуемые файлы (продолжение)
    Удалённые компоненты:
    - CarPage.js (не используется - route использует Feed напрямую)
    - AdminShell.js (создан но не используется - используется AdminView)
    
    Удалённые избыточные layouts:
    - admin/cars/layout.js
    - admin/orders/layout.js
    - admin/orders-calendar/layout.js
    - admin/orders-table/layout.js
    - admin/table/layout.js
    
    Причина: все эти layouts дублируют SessionProvider,
    который уже есть в родительском /admin/layout.js

--- app/admin/components/index.js
+++ app/admin/components/index.js
@@ -2,5 +2,4 @@
 export { default as AdminTopBar } from "./AdminTopBar";
 export { default as AdminLoader } from "./AdminLoader";
 export { default as AdminNotifications } from "./AdminNotifications";
-export { default as AdminShell } from "./AdminShell";
 
